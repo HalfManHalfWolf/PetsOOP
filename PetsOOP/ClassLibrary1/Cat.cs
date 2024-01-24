49496bf9ceb8e903dd2ca63dcad88d882dd4cb4f
HalfManHalfWolf <156145401+HalfManHalfWolf@users.noreply.github.com>
2024-01-23 19:58:36 -0500

    first version
    OOP 2024 example

---
+++ PetsOOP/ClassLibrary1/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Cat : pet
+    {
+        public Cat(string name) : base(name)
+        {
+
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " meows at you.");
+        }
+
+        public void Climb()
+        {
+            Console.WriteLine(Name + " climbs the wall");
+        }
+
+        public void Meet(Chimp newFriend)
+        {
+            Console.WriteLine(Name + " scrathes at " + newFriend.Name);
+        }
+
+        public void Meet(Turtle newfriend)
+        {
+            Console.WriteLine(Name + " scrathes at " + newfriend.Name);
+        }
+    }
+}
---
+++ PetsOOP/ClassLibrary1/pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public abstract class pet
+    {
+        public string Name { get; set; }
+
+        // constructor 
+        public pet(string Name)
+        {
+            Name = Name;
+        }
+        public abstract void MakeSound();
+        public void Showinfo()
+        {
+            Console.WriteLine("Pet Name: " + Name);
+        }
+    }
+
+}
